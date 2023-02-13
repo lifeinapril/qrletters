e1878565c3f8492f02de26e203cdf23dfdb512cb
Abraham Demilade Adetugboboh <80630990+lifeinapril@users.noreply.github.com>
2023-02-13 17:31:47 +0100

    Add files via upload

---
+++ src/components/Sections/Jumbo.js
@@ -0,0 +1,36 @@
+
+
+
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import "../../css/Jumbo.css";
+
+const Jumbo = (props) => {
+return (
+<Container fluid className='box text-left' style={{backgroundImage: `url(${props.image}`}}>
+<br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+        <b style={{fontSize:50,fontFamily:`${props.font || 'tahoma'}`,transition:"all 1s",maxWidth:"600px"}}>
+             <p>{props.title} </p>
+        </b>
+        <p style={{fontSize:25,maxWidth:"600px"}}>
+          {props.body}
+        </p>
+        {
+        props.button ? props.button:null
+        }
+      <br/><br/>
+      <br/><br/>
+</Container>
+
+);
+
+};
+
+export default Jumbo;
