2e133dcca9897a0e63b3edc3b7f87508fc5f99a8
Abraham <brhmdeen@gmail.com>
2023-02-15 18:45:31 +0100

    bb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import Letter from './Letter';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Letter/>);
+import reportWebVitals from './reportWebVitals';
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <BrowserRouter basename="/qrletters">
+        <Letter />
+  </BrowserRouter>
+  </React.StrictMode>
+);
 
+  
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
