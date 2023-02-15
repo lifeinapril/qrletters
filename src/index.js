12554dd219d5e1210dcac7a20291a4b1211ce6b9
Abraham <brhmdeen@gmail.com>
2023-02-15 18:49:13 +0100

    b

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import Letter from './Letter';
 import reportWebVitals from './reportWebVitals';
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <BrowserRouter basename="/qrletters">
-        <Letter />
-  </BrowserRouter>
-  </React.StrictMode>
+      <React.StrictMode>
+          <BrowserRouter basename="/qrletters">
+                <Letter />
+          </BrowserRouter>
+      </React.StrictMode>
 );
 
   
